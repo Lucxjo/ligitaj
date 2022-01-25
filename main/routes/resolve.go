62c6972502e93b1da6cd305c5e433fcb2f55743f
Ludoviko <me@ludoviko.xyz>
2022-01-25 18:23:47 +0000

    Added Resolve function

---
+++ main/routes/resolve.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/lucxjo/ligitaj/main/db"
+	"github.com/lucxjo/ligitaj/main/r"
+)
+
+func ResolveURL(ctx *fiber.Ctx) (error) {
+	url := ctx.Params("url")
+	rdb := db.CreateClient(0)
+	defer rdb.Close()
+
+	value, err := rdb.Get(db.Ctx, url).Result()
+
+	if err == redis.Nil {
+		return ctx.Redirect(r.HomeUrl)
+	} else if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal DB error", "message": err.Error()})
+	}
+
+	rInr := db.CreateClient(1)
+
+	_ = rInr.Incr(db.Ctx, "counter")
+
+	return ctx.Redirect(value, 301)
+}
